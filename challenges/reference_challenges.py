3802027089c033c2eda8847d39af16ad67c0c4bf
Yasiel <toyasiel@gmail.com>
2025-04-01 20:58:56 -0700

    finished reference challenges.

--- challenges/reference_challenges.py
+++ challenges/reference_challenges.py
@@ -2,15 +2,19 @@ def challenge_01():
     apples = 1729
     oranges = 42
     papaya = apples
-    # 1
+    # 1 apples, papaya = 1729
+    # oranges = 42
     apples = apples + oranges
-    # 2
+    # 2 apples = 1771
+    # papaya = 1729
 
 def challenge_02():
     apples = 1729
     oranges = 42
     bananas = [apples, oranges]
-    # 3
+    # 3 apples = 1729
+    # oranges = 42
+    bananas = [1729, 42]
 
 def challenge_03():
     apples = 1729
@@ -22,4 +26,9 @@ def challenge_03():
 def challenge_03_helper(kiwis):
     mangos = 315
     kiwis.append(mangos)
-    # 4
+    # 4 apples = 1729
+    # oranges = #42
+    # bananas  = [1729, 42, 315] the object is a shared list
+    # kiwis is the same list object as bananas, that's what it refers.
+    # mangos = 315
+    # apples, oranges, and mangos are immutable. 
